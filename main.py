d6ac71711f070222536990ce7a79a972b32f9c59
ercanozer <62525303+ercanozer@users.noreply.github.com>
2024-06-13 04:47:11 -0400

    pass vr params

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import random
 from audio_separator.separator import Separator
 from pydub import AudioSegment
 print("Starting the FastAPI server...")
-separator = Separator(output_dir="/workspace/Applio/assets/audios/")
+separator = Separator(output_dir="/workspace/Applio/assets/audios/", vr_params= { "batch_size": 1,"window_size": 512,"aggression": 5,"enable_tta": False,"enable_post_process": False,"post_process_threshold": 0.2,"high_end_process": False })
 separator.load_model("9_HP2-UVR.pth")
 app = FastAPI()
 
